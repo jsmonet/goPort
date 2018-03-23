fc77b388d572ab25a1d80e3ec83d492e513dd19d
Joe franco <joseph.e.franco.-nd@disney.com>
2018-03-23 16:39:07 -0700

    initial commit

---
+++ port.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"time"
+)
+
+var (
+	host    = flag.String("h", "", "Hostname or IP address (string)")
+	port    = flag.Int("p", 22, "TCP port to test (int)")
+	timeout = flag.Int("t", 5, "Timeout for the request in seconds")
+)
+
+func main() {
+	flag.Parse()
+	checkResult := checkPort(*host, *port, *timeout)
+	if checkResult != 0 {
+		fmt.Println("no good")
+	} else {
+		fmt.Println("omg yay")
+	}
+}
+
+func checkPort(address string, port int, timeout int) (result int) {
+	result = 0                                    // explicitly zeroing
+	target := fmt.Sprintf("%v:%v", address, port) // net.DialTimeout requires address:22 formatting, where 22 is just the int port number
+	timeOutSeconds := time.Duration(timeout) * time.Second
+	_, err := net.DialTimeout("tcp", target, timeOutSeconds)
+	if err != nil {
+		result = 2
+	}
+
+	return result
+
+}
